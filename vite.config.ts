89c72183b1c2895c68b960daf7512a9a39628b1c
LOCAL2 <time27535@gmail.com>
2025-10-09 04:19:15 +0700

    fix build-

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'esnext',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: undefined,
